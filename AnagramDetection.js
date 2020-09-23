44250a1e14f4019283475392784610ddcc8e5e2c
Török Attila <torcsiattila93@gmail.com>
2020-09-23 22:17:08 +0200

    Anagram Detection exercise is done (7 kyu)

---
+++ AnagramDetection.js
@@ -0,0 +1,5 @@
+var isAnagram = function (test, original) {
+    return test.toLowerCase().split('').sort().join('') === original.toLowerCase().split('').sort().join('');
+};
+
+console.log(isAnagram("foefet", "toffee"));
