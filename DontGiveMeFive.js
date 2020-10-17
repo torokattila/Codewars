a21738bf640968bd93431ca13ace3765adb51e31
Török Attila <torcsiattila93@gmail.com>
2020-10-17 15:12:40 +0200

    Don't Give Me Five exercise is done (7 kyu)

---
+++ DontGiveMeFive.js
@@ -0,0 +1,20 @@
+/* 
+In this kata you get the start number and the end number of a region and should return the count of all 
+numbers except numbers with a 5 in it. The start and the end number are both inclusive!
+*/
+
+function dontGiveMeFive(start, end) {
+    let counter = 0;
+
+    for (let i = start; i <= end; i++) {
+        if (i.toString().includes('5')) {
+            continue;
+        } else {
+            counter++;
+        }
+    }
+
+    return counter;
+}
+
+console.log(dontGiveMeFive(-4, 17))
