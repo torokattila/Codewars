7470aa8c4fe44dd4b74a6b193348abd52d2da53e
Török Attila <torcsiattila93@gmail.com>
2020-10-21 21:11:39 +0200

    Multiply Characters exercise is done (7 kyu)

---
+++ MultiplyCharacters.js
@@ -0,0 +1,11 @@
+function spam(number) {
+    let resultString = '';
+
+    for (let i = 0; i < number; i++) {
+        resultString += 'hue';
+    }
+
+    return resultString;
+}
+
+console.log(spam(6))
