7df9a0c79da4e29ae17ca475c4d1ff9bd3b8e137
torokattila <torcsiattila93@gmail.com>
2020-10-25 14:55:45 +0100

    Minimum Steps (Array Series #6) exercise is done (7 kyu)

---
+++ MinimumSteps.js
@@ -0,0 +1,27 @@
+function minimumSteps(numbers, value) {
+  numbers = numbers.sort((a, b) => a - b);
+  console.log(numbers);
+  let sum = 0;
+  let counter = 0;
+  let tempSum = 0;
+  
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+
+    if (sum <= value) {
+        tempSum += numbers[i];
+        counter++;
+    }
+  }
+
+
+  if (tempSum == value) {
+      return counter - 1;
+  } else {
+      return counter;
+  }
+
+  
+}
+
+console.log(minimumSteps([ 4, 6, 3 ], 7))
