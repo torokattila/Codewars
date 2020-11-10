91fc4b69891f06c0b8ccb4602742a8c2789c0d42
Török Attila <torcsiattila93@gmail.com>
2020-11-10 21:40:42 +0100

    First Reverse Try exercise is done (7 kyu)

---
+++ FirstReverseTry.js
@@ -0,0 +1,13 @@
+/*Given an array arr, swap its first and last elements and return the resulting array.*/
+
+function firstReverseTry(arr) {
+    if (arr.length == 0) {
+        return [];
+    }
+    
+    [arr[0], arr[arr.length - 1]] = [arr[arr.length - 1], arr[0]];
+
+    return arr;
+}
+
+console.log(firstReverseTry([1, 2, 3, 4, 5]));
