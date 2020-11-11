9ac4693bf12f4e1b04f6a51843355b2a1716bd38
Török Attila <torcsiattila93@gmail.com>
2020-11-11 21:49:21 +0100

    Simple Equation Reversal exercise is done (7 kyu)

---
+++ SimpleEquationReversal.js
@@ -0,0 +1,9 @@
+/*Given a mathematical equation that has *,+,-,/, reverse it as follows:
+solve("100*b/y") = "y/b*100"
+solve("a+b-c/d*30") = "30*d/c-b+a"*/
+
+function solve(eq) {
+    return eq.split(/([\*+-/])/).reverse().join('');
+}
+
+console.log(solve("100*b/y"))
