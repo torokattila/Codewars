525b9f631cc9320779c06052b535bb235a50eb9b
Török Attila <torcsiattila93@gmail.com>
2020-11-29 11:31:13 +0100

    Filter the number exercise is done (7 kyu)

---
+++ FilterTheNumber.js
@@ -0,0 +1,14 @@
+/*Oh no! The number has been mixed up with the text. Your goal is to retreive the number from the text, 
+can you return the number back to it's original state?
+Task
+Your task is to return a number from a string.
+Details
+You will be given a string of numbers and letters mixed up, you have to return all the numbers in that 
+string in the order they occur.*/
+
+var FilterString = function (value) {
+    value = value.replace(/[a-z]/gi, '');
+    return parseInt(value);
+}
+
+console.log(FilterString('a1b2c3'));
