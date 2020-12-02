b6db479ae4aa34f388249cee3e5f6b9c174bc33e
Török Attila <torcsiattila93@gmail.com>
2020-12-02 21:12:51 +0100

    Coding Meetup #10 exercise is done (6 kyu)

---
+++ CodingMeetup10.js
@@ -0,0 +1,26 @@
+/*write a function that adds the username property to each object in the input array:
+The value of the username property is composed by concatenating:
+
+firstName in lower-case;
+first letter of the lastName in lower-case; and
+the birth year which for the purpose of this kata is calculated simply by subtracting age from the current 
+year. Please make sure that your function delivers the correct birth year irrespective of when it will be 
+executed, for example it should also work correctly for a meetup organised in 10-years-time. The example 
+above assumes that the function is run in year 2020.*/
+
+
+function addUsername(list) {
+    list.forEach(developer => {
+        const currentYear = new Date().getFullYear();
+        developer = developer.username = developer.firstName.toLowerCase() + developer.lastName.charAt(0).toLowerCase() + (currentYear - developer.age).toString();
+    });
+
+    return list;
+}
+
+const list1 = [
+    { firstName: 'Emily', lastName: 'N.', country: 'Ireland', continent: 'Europe', age: 30, language: 'Ruby' },
+    { firstName: 'Nor', lastName: 'E.', country: 'Malaysia', continent: 'Asia', age: 20, language: 'Clojure' }
+];
+
+console.log(addUsername(list1));
