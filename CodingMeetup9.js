4de7d4c0545271accfd084f76c8df9e44418be6e
torokattila <torcsiattila93@gmail.com>
2021-05-15 14:08:41 +0200

    Coding Meetup #9 - Higher-Order Functions Series - Is the meetup age-diverse? exercise is done (6 kyu)

---
+++ CodingMeetup9.js
@@ -0,0 +1,129 @@
+/*You will be given an array of objects (associative arrays in PHP) representing data about developers who have signed up 
+to attend the next coding meetup that you are organising.
+
+Your task is to return:
+
+true if developers from all of the following age groups have signed up: teens, twenties, thirties, forties, 
+fifties, sixties, seventies, eighties, nineties, centenarian (at least 100 years young).
+false otherwise.
+For example, given the following input array:
+
+var list1 = [
+  { firstName: 'Harry', lastName: 'K.', country: 'Brazil', continent: 'Americas', age: 19, language: 'Python' },
+  { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 29, language: 'JavaScript' },
+  { firstName: 'Jing', lastName: 'X.', country: 'China', continent: 'Asia', age: 39, language: 'Ruby' },
+  { firstName: 'Noa', lastName: 'A.', country: 'Israel', continent: 'Asia', age: 40, language: 'Ruby' },
+  { firstName: 'Andrei', lastName: 'E.', country: 'Romania', continent: 'Europe', age: 59, language: 'C' },
+  { firstName: 'Maria', lastName: 'S.', country: 'Peru', continent: 'Americas', age: 60, language: 'C' },
+  { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 75, language: 'Python' },
+  { firstName: 'Chloe', lastName: 'K.', country: 'Guernsey', continent: 'Europe', age: 88, language: 'Ruby' },
+  { firstName: 'Viktoria', lastName: 'W.', country: 'Bulgaria', continent: 'Europe', age: 98, language: 'PHP' },
+  { firstName: 'Piotr', lastName: 'B.', country: 'Poland', continent: 'Europe', age: 128, language: 'JavaScript' }
+];
+your function should return true as there is at least one developer from each age group.
+
+Notes:
+
+The input array will always be valid and formatted as in the example above.
+Age is represented by a number which can be any positive integer up to 199.*/
+
+function isAgeDiverse(list) {
+	const agesArray = list.map(person => person.age);
+    const isTeen = agesArray.some(age => age > 0 && age <= 19);
+    const isTwenties = agesArray.some(age => age >= 20 && age <= 29);
+    const isThirties = agesArray.some(age => age >= 30 && age <= 39);
+    const isForties = agesArray.some(age => age >= 40 && age <= 49);
+    const isFifties = agesArray.some(age => age >= 50 && age <= 59);
+    const isSixties = agesArray.some(age => age >= 60 && age <= 69);
+    const isSeventies = agesArray.some(age => age >= 70 && age <= 79);
+    const isEighties = agesArray.some(age => age >= 80 && age <= 89);
+    const isNineTies = agesArray.some(age => age >= 90 && age <= 99);
+    const isCentenarian = agesArray.some(age => age >= 100 && age <= 199);
+
+    return isTeen && isTwenties && isThirties && isForties && isFifties && isSixties && isSeventies && isEighties && isNineTies && isCentenarian;
+}
+
+const list1 = [
+	{
+		firstName: "Harry",
+		lastName: "K.",
+		country: "Brazil",
+		continent: "Americas",
+		age: 19,
+		language: "Python"
+	},
+	{
+		firstName: "Kseniya",
+		lastName: "T.",
+		country: "Belarus",
+		continent: "Europe",
+		age: 29,
+		language: "JavaScript"
+	},
+	{
+		firstName: "Jing",
+		lastName: "X.",
+		country: "China",
+		continent: "Asia",
+		age: 39,
+		language: "Ruby"
+	},
+	{
+		firstName: "Noa",
+		lastName: "A.",
+		country: "Israel",
+		continent: "Asia",
+		age: 40,
+		language: "Ruby"
+	},
+	{
+		firstName: "Andrei",
+		lastName: "E.",
+		country: "Romania",
+		continent: "Europe",
+		age: 59,
+		language: "C"
+	},
+	{
+		firstName: "Maria",
+		lastName: "S.",
+		country: "Peru",
+		continent: "Americas",
+		age: 60,
+		language: "C"
+	},
+	{
+		firstName: "Lukas",
+		lastName: "X.",
+		country: "Croatia",
+		continent: "Europe",
+		age: 75,
+		language: "Python"
+	},
+	{
+		firstName: "Chloe",
+		lastName: "K.",
+		country: "Guernsey",
+		continent: "Europe",
+		age: 88,
+		language: "Ruby"
+	},
+	{
+		firstName: "Viktoria",
+		lastName: "W.",
+		country: "Bulgaria",
+		continent: "Europe",
+		age: 98,
+		language: "PHP"
+	},
+	{
+		firstName: "Piotr",
+		lastName: "B.",
+		country: "Poland",
+		continent: "Europe",
+		age: 128,
+		language: "JavaScript"
+	}
+];
+
+console.log(isAgeDiverse(list1));
