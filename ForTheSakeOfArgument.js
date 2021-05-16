0537068e183cb59b029f7b084219f857c793e1e1
torokattila <torcsiattila93@gmail.com>
2021-05-16 11:25:52 +0200

    For the sake of argument exercise is done (7 kyu)

---
+++ ForTheSakeOfArgument.js
@@ -0,0 +1,23 @@
+/*Write a function named numbers that returns true if all the parameters it is passed are of the Number type. 
+Otherwise, the function should return false. The function should accept any number of parameters.
+
+Example usage:
+
+numbers(1, 4, 3, 2, 5); // true
+numbers(1, "a", 3); // false
+numbers(1, 3, NaN); // true*/
+
+function numbers() {
+    const argsArray = [...arguments];
+    let result = true;
+
+    argsArray.forEach(arg => {
+        if (typeof arg != 'number') {
+            result = false;
+        }
+    })
+
+    return result;
+}
+
+console.log(numbers(1, 3, NaN));
