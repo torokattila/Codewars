1a63e823e7faad393ac950aa0e0fdbb1d5537831
torokattila <torcsiattila93@gmail.com>
2021-05-24 14:08:38 +0200

    Sum of all arguments exercise is done (7 kyu)

---
+++ SumOfAllArguments.js
@@ -0,0 +1,15 @@
+/*Write a function that finds the sum of all its arguments.
+
+eg:
+
+sum(1, 2, 3) // => 6
+sum(8, 2) // => 10
+sum(1, 2, 3, 4, 5) // => 15
+Note that Python's function name is sum_args, as to avoid confusion with the preexistsing sum function. 
+Ruby's Array#sum has been removed to make it a bit more interesting!*/
+
+function sum() {
+	return [...arguments].reduce((accum, current) => accum + current);
+}
+
+console.log(sum(1, 2, 3));
