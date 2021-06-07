80a8bf6df640e01e281b39b6cf76018c206ceb65
torokattila <torcsiattila93@gmail.com>
2021-06-07 20:33:24 +0200

    Find The Duplicated Number in a Consecutive Unsorted List exercise is done (7 kuy)

---
+++ FindTheDuplicateNumberInAConsecutiveUnsortedList.js
@@ -0,0 +1,27 @@
+/*You are given an array of n+1 integers 1 through n. In addition there is a single duplicate integer.
+
+The array is unsorted.
+
+An example valid array would be [3, 2, 5, 1, 3, 4]. It has the integers 1 through 5 and 3 is duplicated. 
+[1, 2, 4, 5, 5] would not be valid as it is missing 3.
+
+You should return the duplicate value as a single integer.*/
+
+function findDup(arr) {
+	let occurrences = {};
+	let result = 0;
+
+	arr.forEach(item => {
+		return occurrences[item] ? occurrences[item]++ : (occurrences[item] = 1);
+	});
+
+	for (const [key, value] of Object.entries(occurrences)) {
+		if (value == 2) {
+			result = parseInt(key);
+		}
+	}
+
+	return result;
+}
+
+console.log(findDup([3, 2, 5, 1, 3, 4]));
