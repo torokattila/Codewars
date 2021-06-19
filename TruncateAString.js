d962f2c7f958656c96afd4301215821178a313a1
torokattila <torcsiattila93@gmail.com>
2021-06-19 16:50:27 +0200

    Truncate a string! exercise is done (7 kyu)

---
+++ TruncateAString.js
@@ -0,0 +1,24 @@
+/*Truncate the given string (first argument) if it is longer than the given maximum length (second argument). 
+Return the truncated string with a "..." ending.
+
+Note that inserting the three dots to the end will add to the string length.
+
+However, if the given maximum string length num is less than or equal to 3, 
+then the addition of the three dots does not add to the string length in determining the truncated string.
+
+Examples
+('codewars', 9)  ==>  'codewars'
+('codewars', 7)  ==>  'code...'
+('codewars', 2)  ==>  'co...'*/
+
+function truncateString(str, num) {
+    if (str.length <= num) {
+        return str;
+    } else if (str.length > num && num <= 3) {
+        return `${str.substring(0, num)}...`;
+    } else {
+        return `${str.substring(0, num - 3)}...`;
+    }
+}
+
+console.log(truncateString('codewars', 7));
