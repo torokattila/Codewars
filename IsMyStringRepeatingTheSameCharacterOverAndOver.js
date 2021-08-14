307ec03d7eb68859cbe9c75b3267e7bf4d75cb3b
torokattila <torcsiattila93@gmail.com>
2021-08-14 14:00:38 +0200

    Is my string repeating the same character over and over? exercise is done (7 kyu)

---
+++ IsMyStringRepeatingTheSameCharacterOverAndOver.js
@@ -0,0 +1,21 @@
+/*Your task is very simple:
+Write a function hasOneChar returning:
+
+true if the given string contains the same character repeated all along the string
+false otherwise.
+For instance:
+
+hasOneChar('aaaaa'); //true
+hasOneChar('aaaab'); //false
+hasOneChar('bbbbb'); //true
+hasOneChar('bbabb'); //false
+Of course, what comes in mind first is a loop. But the purpose of this Kata is to test your creativity.
+You have to achieve it without using any loops.
+
+Corner cases: the function hasOneChar should return true if the string contains one single character.*/
+
+function hasOneChar(s) {
+	return [...new Set(s.split(''))].length === 1;
+}
+
+console.log(hasOneChar("aaaaaa"));
