f341c49ab2f3336317821e62825aa9224706d6bc
torokattila <torcsiattila93@gmail.com>
2021-08-20 15:37:22 +0200

    Palindrome Strings exercise is done (8 kyu)

---
+++ PalindromeStrings.js
@@ -0,0 +1,17 @@
+/*Palindrome strings
+A palindrome is a word, phrase, number, or other sequence of characters which reads the same backward or forward. 
+This includes capital letters, punctuation, and word dividers.
+
+Implement a function that checks if something is a palindrome.
+
+Examples
+isPalindrome("anna")   ==> true
+isPalindrome("walter") ==> false
+isPalindrome(12321)    ==> true
+isPalindrome(123456)   ==> false*/
+
+function isPalindrome(line) {
+    return `${line}`.split('').reverse().join('') === `${line}`;
+}
+
+console.log(isPalindrome("anna"));
