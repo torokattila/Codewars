0dc040479ae956c695945233302cfdc7dba1640b
torokattila <torcsiattila93@gmail.com>
2021-09-16 19:58:09 +0200

    Uncollapse Digits exercise is done (6 kyu)

---
+++ UncollapseDigits.js
@@ -0,0 +1,20 @@
+/*You will be given a string of English digits "stuck" together, like this:
+
+"zeronineoneoneeighttwoseventhreesixfourtwofive"
+
+Your task is to split the string into separate digits:
+
+"zero nine one one eight two seven three six four two five"
+
+Examples
+"three"              -->  "three"
+"eightsix"           -->  "eight six"
+"fivefourseven"      -->  "five four seven"
+"ninethreesixthree"  -->  "nine three six three"
+"fivethreefivesixthreenineonesevenoneeight"  -->  "five three five six three nine one seven one eight"*/
+
+function uncollapse(digits) {
+    return digits.match(/zero|one|two|three|four|five|six|seven|eight|nine/g).join(' ');
+}
+
+console.log(uncollapse("zeronineoneoneeighttwoseventhreesixfourtwofive"));
