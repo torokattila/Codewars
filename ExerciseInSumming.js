916100dddaa6904acbe1f4933f8b0e5b07a0ff50
torokattila <torcsiattila93@gmail.com>
2021-09-19 11:36:14 +0200

    Exercise in Summing exercise is done (6 kyu)

---
+++ ExerciseInSumming.js
@@ -0,0 +1,44 @@
+/*Your task is to finish two functions, minimumSum and maximumSum, that take 2 parameters:
+
+values: an array of integers with an arbitrary length; may be positive and negative
+n: how many integers should be summed; always 0 or bigger
+Example:
+var values = [5, 4, 3, 2, 1];
+minimumSum(values, 2); // should return 1+2 = 3
+maximumSum(values, 3); // should return 3+4+5 = 12
+All values given to the functions will be integers. Also take care of the following special cases:
+
+if values is empty, both functions should return 0
+if n is 0, both functions should also return 0
+if n is larger than values's length, use the length instead.*/
+
+function minimumSum(values, n) {
+    if (values.length === 0 || n === 0) return 0;
+
+	const sortedValues = values.sort((a, b) => a - b);
+    const length = n > sortedValues.length ? sortedValues.length : n;
+    let sum = 0;
+
+    for (let i = 0; i < length; i++) {
+        sum += sortedValues[i];
+    }
+
+    return sum;
+}
+
+function maximumSum(values, n) {
+    if (values.length === 0 || n === 0) return 0;
+
+	const sortedValues = values.sort((a, b) => b - a);
+    const length = n > sortedValues.length ? sortedValues.length : n;
+    let sum = 0;
+
+    for (let i = 0; i < length; i++) {
+        sum += sortedValues[i];
+    }
+
+    return sum;
+}
+
+console.log(minimumSum([5, 4, 3, 2, 1], 2));
+console.log(maximumSum([5, 4, 3, 2, 1], 3));
