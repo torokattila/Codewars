ff5f9186c3ceb3b909bc7462f37e000c68e126c1
torokattila <torcsiattila93@gmail.com>
2021-09-29 19:51:14 +0200

    Trim a String exercise is done (6 kyu)

---
+++ TrimAString.js
@@ -0,0 +1,11 @@
+/*Define a function trim (JavaScript: method String.prototype.trim) which 
+removes all forms of leading and trailing whitespace from a given string. 
+Please refer to the Sample Tests for more examples.
+
+Note: The built-in trim, trimLeft && trimRight methods has been disabled.*/
+
+String.prototype.trim = function() {
+	return this.replace(/^\s+|\s+$/g, "");
+};
+
+console.log("      foo      ".trim());
