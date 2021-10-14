5b5df0fa326cf9b0b4f21f2ffc6e03d14ecbc241
torokattila <torcsiattila93@gmail.com>
2021-10-14 20:08:00 +0200

    Cyclops numbers exercise is done (7 kyu)

---
+++ CyclopsNumbers.js
@@ -0,0 +1,48 @@
+/*A cyclops number is a number in binary that is made up of all 1's, with one 0 in the exact middle. 
+That means all cyclops numbers must have an odd number of digits for there to be an exact middle.
+A couple examples:
+101
+11111111011111111
+You must take an input, n, that will be in decimal format (base 10), then return True if that number 
+wil be a cyclops number when converted to binary, or False if it won't.
+Assume n will be a positive integer.
+
+A test cases with the process shown:
+
+cyclops (5)
+"""5 in biinary"""
+"0b101"
+"""because 101 is made up of all "1"s with a "0" in the middle, 101 is a cyclops number"""
+return True
+
+cyclops(13)
+"""13 in binary"""
+"0b1101"
+"""because 1101 has an even number of bits, it cannot be a cyclops"""
+return False
+
+cyclops(17)
+"""17 in binary"""
+"0b10001"
+"""Because 10001 has more than 1 "0" it cannot be a cyclops number"""
+return False*/
+
+function cyclops(n) {
+	const numberInBinary = n.toString(2);
+	if (numberInBinary.length % 2 === 0 || n === 1) return false;
+
+	const onlyOneZero = numberInBinary.split('').filter(char => char === '0').length === 1;
+	const onlyOneOne = numberInBinary.split('').filter(char => char === '1').length === 1;
+
+	const middleCharacter = numberInBinary[Math.floor(numberInBinary.length / 2)];
+
+	if (onlyOneZero) {
+		return middleCharacter === '0';
+	} else if (onlyOneOne) {
+		return middleCharacter === '1';
+	} else {
+        return false;
+    }
+}
+
+console.log(cyclops(33550334));
