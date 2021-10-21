b758788eead574ecc94853aa40df5a34f76bed89
torokattila <torcsiattila93@gmail.com>
2021-10-21 20:31:19 +0200

    Trim method exercise is done (7 kyu)

---
+++ TrimMethod.js
@@ -0,0 +1,25 @@
+/*In this kata we are going to extend the trim method to be able to remove any character(upper and lower cases) 
+from both sides of a string.
+Task:
+Create a method called trim which removes the passed parameter c(upper and lower cases) 
+from the leading and tailing of the string.
+
+Note:
+1- If no parameter is being passed you need to remove the leading and tailing spaces.
+2- The passed parameter will only be alphabet(upper and lower cases) characters.
+Examples:
+string = "LLLHello Worldlll" and c = "l" => "Hello World"
+
+string = "Visca ElbarcaXxX" and c = "X" => "Visca Elbarca"*/
+
+String.prototype.trim = function(c) {
+    if (!c) {
+        return this.replace(/^\s+|\s+$/g, '');
+    } else {
+        const regex = new RegExp(`^${c}+|${c}+$`, 'gi');
+
+        return this.replace(regex, '');
+    }
+};
+
+console.log('LLLHello Worldlll'.trim('l'));
