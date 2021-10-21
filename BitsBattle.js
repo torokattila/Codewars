1b4c2e77e4686c860606e287dbe3b5247828f252
torokattila <torcsiattila93@gmail.com>
2021-10-21 19:37:37 +0200

    Bits Battle exercise is done (7 kyu)

---
+++ BitsBattle.js
@@ -0,0 +1,44 @@
+/*The odd and even numbers are fighting against each other!
+
+You are given a list of positive integers. The odd numbers from the list will fight using their 1 bits from their binary representation, while the even numbers will fight using their 0 bits. If present in the list, number 0 will be neutral, hence not fight for either side.
+
+You should return:
+
+odds win if number of 1s from odd numbers is larger than 0s from even numbers
+evens win if number of 1s from odd numbers is smaller than 0s from even numbers
+tie if equal, including if list is empty
+Please note that any prefix that might appear in the binary representation, e.g. 0b, should not be counted towards the battle.
+
+Example:
+For an input list of [5, 3, 14]:
+
+odds: 5 and 3 => 101 and 11 => four 1s
+evens: 14 => 1110 => one 0
+Result: odds win the battle with 4-1*/
+
+function bitsBattle(numbers) {
+	if (numbers.length === 0) return 'tie';
+
+	let evenCounter = 0;
+	let oddCounter = 0;
+
+	numbers.forEach(number => {
+		if (number % 2 === 0) {
+			evenCounter += number
+				.toString(2)
+				.split('')
+				.filter(char => char === '0').length;
+		} else if (number % 2 !== 0) {
+			oddCounter += number
+				.toString(2)
+				.split('')
+				.filter(char => char === '1').length;
+		}
+	});
+
+	return evenCounter > oddCounter
+		? 'evens win'
+		: oddCounter > evenCounter ? 'odds win' : 'tie';
+}
+
+console.log(bitsBattle([ 65, 156, 9, 169, 62, 5, 48, 0 ]));
