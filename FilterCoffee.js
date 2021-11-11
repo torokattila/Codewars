d88019e245e07c324f86c04fb30354d678119b9a
torokattila <torcsiattila93@gmail.com>
2021-11-11 21:15:50 +0100

    Filter Coffee exercise is done (7 kyu)

---
+++ FilterCoffee.js
@@ -0,0 +1,20 @@
+/*You love coffee and want to know what beans you can afford to buy it.
+
+The first argument to your search function will be a number which represents your budget.
+
+The second argument will be an array of coffee bean prices.
+
+Your 'search' function should return the stores that sell coffee within your budget.
+
+The search function should return a string of prices for the coffees beans you can afford. 
+The prices in this string are to be sorted in ascending order.*/
+
+function search(budget, prices) {
+	return prices
+		.filter(price => price <= budget)
+		.sort((a, b) => a - b)
+		.map(num => `${num}`)
+		.join(',');
+}
+
+console.log(search(14, [7, 3, 23, 9, 14, 20, 7]));
