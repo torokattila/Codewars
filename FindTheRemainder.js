39a4b07d692994267fa1d4073846459ad0cb4901
torokattila <torcsiattila93@gmail.com>
2021-11-29 19:32:28 +0100

    Find the Remainder exercise is done (8 kyu)

---
+++ FindTheRemainder.js
@@ -0,0 +1,19 @@
+/*Write a function that accepts two arguments and returns the remainder after dividing the larger number by the smaller number.
+
+Division by zero should return NaN. (
+
+Example
+remainder(17, 5) -> 2
+remainder(13, 72) -> remainder(72,13) -> 7 (The larger number should divide the smaller number)
+remainder(1, 0) -> NaN (Division by 0)
+remainder(0, 0) -> NaN (Divison by 0)
+Input
+Arguments will both be integers: positive, negative, or neutral (0)*/
+
+function remainder(a, b) {
+	if (a === 0 || b === 0) return NaN;
+
+    return a > b ? a % b : b % a;
+}
+
+console.log(remainder(13, 72));
