2f40993d445aec539f1b18ab9b4df445b299e30a
torokattila <torcsiattila93@gmail.com>
2021-12-01 20:18:22 +0100

    Sum without highest and lowest number exercise is done (8 kyu)

---
+++ SumWithoutHighestAndLowestNumber.js
@@ -0,0 +1,22 @@
+/*Sum all the numbers of the array (in F# and Haskell you get a list) except the highest and the lowest element 
+(the value, not the index!).
+(The highest/lowest element is respectively only one element at each edge, even if there are more than one with the same value!)
+
+Example:
+
+{ 6, 2, 1, 8, 10 } => 16
+{ 1, 1, 11, 2, 3 } => 6
+
+If array is empty, null or None, or if only 1 Element exists, return 0.
+Note:In C++ instead null an empty vector is used. In C there is no null. ;-)*/
+
+function sumArray(array) {
+	if (!array || array.length <= 2) return 0;
+
+	return array
+		.sort((a, b) => a - b)
+		.slice(1, -1)
+		.reduce((accum, current) => accum + current);
+}
+
+console.log(sumArray([6, 2, 1, 8, 10]));
