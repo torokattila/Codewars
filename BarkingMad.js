de80781038bce48de0ebf96ba3b09571d10cddcb
torokattila <torcsiattila93@gmail.com>
2021-12-07 19:49:28 +0100

    Barking mad exercise is done (8 kyu)

---
+++ BarkingMad.js
@@ -0,0 +1,20 @@
+/*Teach snoopy and scooby doo how to bark using object methods. Currently only snoopy can bark and not scooby doo.
+
+snoopy.bark(); // return "Woof"
+scoobydoo.bark(); // undefined
+Use method prototypes to enable all Dogs to bark.*/
+
+function Dog(breed) {
+	this.breed = breed;
+
+    this.bark = function() {
+        return 'Woof';
+    }
+}
+
+var snoopy = new Dog('Beagle');
+
+console.log(snoopy.bark());
+
+var scoobydoo = new Dog('Great Dane');
+console.log(scoobydoo.bark())
