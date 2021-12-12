6f9567ffc70aab1e4c01c26d4ac0eb73ecba6425
torokattila <torcsiattila93@gmail.com>
2021-12-12 13:29:30 +0100

    #~For Kids~# Date decryption. exercise is done (7 kyu)

---
+++ ForKidsDateEncryption.js
@@ -0,0 +1,34 @@
+/*#~For Kids Challenges~#
+This kata is the reverse of my another: See Here!
+Here your task is to write a function that takes an encrypted date in format(ASCII CODE = AC) ACAC-AC-AC(String) and 
+return it in an deciphered format (normal date in YYYY-mm-dd), to do this you will break the string of encrypted date, 
+for each character of date(except '-') translate it to ASCII code subtract 50 of it and return the result translated 
+according to ASCII table with the '-' between it.
+Example: "FC-3-G" ->(ASCII Translation)-> You will get 70 67 - 51 - 71 (for each of then -50)-> 20 17 - 1 - 21 -> "2017-01-21"
+
+"Et->-9" -> "1966-12-07"*/
+
+function translateDate(dateStr) {
+	const parts = dateStr.split('-');
+	const year = parts[0]
+		.split('')
+		.map(
+			char =>
+				char.charCodeAt() - 50 < 10
+					? `0${char.charCodeAt() - 50}`
+					: `${char.charCodeAt() - 50}`
+		)
+		.join('');
+	const month =
+		parts[1].charCodeAt() - 50 < 10
+			? `0${parts[1].charCodeAt() - 50}`
+			: `${parts[1].charCodeAt() - 50}`;
+	const day =
+		parts[2].charCodeAt() - 50 < 10
+			? `0${parts[2].charCodeAt() - 50}`
+			: `${parts[2].charCodeAt() - 50}`;
+
+	return `${year}-${month}-${day}`;
+}
+
+console.log(translateDate('F4-4-4'));
