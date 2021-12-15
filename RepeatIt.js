e12992585893133a806806b6d097f248c229af45
torokattila <torcsiattila93@gmail.com>
2021-12-15 21:01:56 +0100

    RepeatIt.js

---
+++ RepeatIt.js
@@ -0,0 +1,22 @@
+/*Create a function that takes a string and an integer (n).
+
+The function should return a string that repeats the input string n number of times.
+
+If anything other than a string is passed in you should return "Not a string"
+
+Example
+"Hi", 2 --> "HiHi"
+1234, 5 --> "Not a string"*/
+
+var repeatIt = function(str, n) {
+    if (typeof str !== 'string') return 'Not a string';
+    let result = '';
+
+    for (let i = 0; i < n; i++) {
+        result += str;
+    }
+
+    return result;
+};
+
+console.log(repeatIt('*', 3));
