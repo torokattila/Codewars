1b184f722048efc51a164d9a9a7dda4daec83a93
torokattila <torcsiattila93@gmail.com>
2022-01-15 12:28:28 +0100

    Array Appender exercise is done (7 kyu)

---
+++ ArrayAppender.js
@@ -0,0 +1,21 @@
+/*Write a function that appends the items from sequence 2 onto sequence 1, returning the newly formed sequence. 
+Your function should also be able to handle nested sequences.
+
+All inputs will be arrays/nested arrays.
+
+For example:
+
+['a','b','c'], [1,2,3]     --> ['a','b','c',1,2,3]
+[['x','x'],'B'], ['c','D'] --> [['x','x'],'B','c','D']*/
+
+function appendArrays(arr1, arr2) {
+    const resultArray = [];
+
+    [...arr1, ...arr2].forEach(item => {
+        resultArray.push(item);
+    });
+
+    return resultArray;
+}
+
+console.log(appendArrays([['x','x'],'B'], ['c','D'] ));
