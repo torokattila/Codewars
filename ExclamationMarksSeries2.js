e4b3921b4116d60e9054ef019ea16653695da0e4
Attila Török <attilatorok@pop-os.localdomain>
2022-01-22 15:54:01 +0100

    Exclamation marks series #2: Remove all exclamation marks from the end of the sentence exercise is done (8 kyu)

---
+++ ExclamationMarksSeries2.js
@@ -0,0 +1,15 @@
+/*Remove all exclamation marks from the end of sentence.
+
+Examples
+remove("Hi!") === "Hi"
+remove("Hi!!!") === "Hi"
+remove("!Hi") === "!Hi"
+remove("!Hi!") === "!Hi"
+remove("Hi! Hi!") === "Hi! Hi"
+remove("Hi") === "Hi"*/
+
+function remove(string) {
+    return string.replace(/!+$/gm, '');
+}
+
+console.log(remove('Hi! Hi!'));
