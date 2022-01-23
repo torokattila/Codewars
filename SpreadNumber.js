ca71e81da93a22bc2d1d5d2245b3efd9f6c83f34
Attila Török <attilatorok@pop-os.localdomain>
2022-01-23 17:02:23 +0100

    Spread number exercise is done (6 kyu)

---
+++ SpreadNumber.js
@@ -0,0 +1,24 @@
+/*It would be cool if we could create arrays filled by numbers from 1 to n like this arr = [...n].
+
+Your task is to do something with Number to make it possible.
+
+In your code you shouldn't do anything else, only modify Number.
+
+Example
+[...1] => [1]
+
+[...2] => [1, 2]
+
+[...3] => [1, 2, 3]
+
+[...4] => [1, 2, 3, 4]
+
+[...5] => [1, 2, 3, 4, 5]*/
+
+Number.prototype[Symbol.iterator] =  function* (i = 1) {
+    while (i <= this) {
+        yield i++;
+    }
+}
+
+console.log([...5]);
