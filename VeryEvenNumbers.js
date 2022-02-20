8ef7edbe646f23c2f91e09c4ae2e806b495a575a
Attila Török <attilatorok@pop-os.localdomain>
2022-02-20 16:46:42 +0100

    Very Even Numbers exercise is done (7 kyu)

---
+++ VeryEvenNumbers.js
@@ -0,0 +1,32 @@
+/*Write a function that returns true if the number is a "Very Even" number.
+
+If a number is a single digit, then it is simply "Very Even" if it itself is even.
+
+If it has 2 or more digits, it is "Very Even" if the sum of it's digits is "Very Even".
+
+#Examples:
+
+input(88) => returns false -> 8 + 8 = 16 -> 1 + 6 = 7 => 7 is odd 
+
+input(222) => returns true
+
+input(5) => returns false
+
+input(841) => returns true -> 8 + 4 + 1 = 13 -> 1 + 3 => 4 is even
+Note: The numbers will always be 0 or positive integers!*/
+
+function isVeryEvenNumber(n) {
+    if (`${n}`.length === 1 && n % 2 === 0) {
+        return true;
+    } else if (`${n}`.length >= 2) {
+        return isVeryEvenNumber(
+            `${n}`
+                .split('')
+                .reduce((accum, current) => Number(accum) + Number(current))
+        );
+    } else {
+        return false;
+    }
+}
+
+console.log(isVeryEvenNumber(88));
