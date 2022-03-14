d717911d1120ef9186623a642c44cec54875f7dc
torokattila <torcsiattila93@gmail.com>
2022-03-14 14:45:07 +0100

    Colour Association kata is done (7 kyu)

---
+++ ColourAssociation.js
@@ -0,0 +1,30 @@
+/*Colour plays an important role in our lifes. Most of us like this colour better then another. 
+User experience specialists believe that certain colours have certain psychological meanings for us.
+
+You are given a 2D array, composed of a colour and its 'common' association in each array element. 
+The function you will write needs to return the colour as 'key' and association as its 'value'.
+
+For example:
+
+var array = [["white", "goodness"], ...] //returns [{white: 'goodness'}, ...]*/
+
+function colourAssociation(array) {
+    return array.map((item) => {
+        const object = {};
+        const key = item[0];
+        const value = item[1];
+
+        object[key] = value;
+
+        return object;
+    });
+}
+
+console.log(
+    colourAssociation([
+        ['red', 'energy'],
+        ['yellow', 'creativity'],
+        ['brown', 'friendly'],
+        ['green', 'growth'],
+    ])
+);
