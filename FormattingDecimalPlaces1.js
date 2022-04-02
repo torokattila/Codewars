47bf9eaa3ec98beebb2e87ba44bedadd91ab4a2a
torokattila <torcsiattila93@gmail.com>
2022-04-02 16:14:27 +0200

    Formatting decimal places #1 kata is done (7 kyu)

---
+++ FormattingDecimalPlaces1.js
@@ -0,0 +1,22 @@
+/*Each floating-point number should be formatted that only the first two decimal places are returned. 
+You don't need to check whether the input is a valid number because only valid numbers are used in the tests.
+
+Don't round the numbers! Just cut them after two decimal places!
+
+Right examples:  
+32.8493 is 32.84  
+14.3286 is 14.32
+
+Incorrect examples (e.g. if you round the numbers):  
+32.8493 is 32.85  
+14.3286 is 14.33*/
+
+function twoDecimalPlaces(number) {
+    const splittedNumber = `${number}`.split('.');
+    const whole = splittedNumber[0];
+    const float = splittedNumber[1];
+
+    return Number(`${whole}.${float.substring(0, 2)}`);
+}
+
+console.log(twoDecimalPlaces(4.653725356));
