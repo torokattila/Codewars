896ff73635fe6559ffeacec109abe0e0ecf4301d
torokattila <torcsiattila93@gmail.com>
2022-04-23 17:42:09 +0200

    Coding Meetup #17 - Higher-Order Functions Series - Sort by programming language kata is done (7 kyu)

---
+++ CodingMeetup17SortByProgrammingLanguage.js
@@ -0,0 +1,80 @@
+/*You will be given an array of objects representing data about developers who have signed up to attend 
+the next coding meetup that you are organising.
+
+Given the following input array:
+
+var list1 = [  
+  { firstName: 'Nikau', lastName: 'R.', country: 'New Zealand', continent: 'Oceania', age: 39, language: 'Ruby' },
+  { firstName: 'Precious', lastName: 'G.', country: 'South Africa', continent: 'Africa', age: 22, language: 'JavaScript' },
+  { firstName: 'Maria', lastName: 'S.', country: 'Peru', continent: 'Americas', age: 30, language: 'C' },
+  { firstName: 'Agustin', lastName: 'V.', country: 'Uruguay', continent: 'Americas', age: 19, language: 'JavaScript' }
+];
+Write a function that returns the array sorted alphabetically by the programming language. 
+In case there are some developers that code in the same language, sort them alphabetically by the first name:
+
+[ 
+  { firstName: 'Maria', lastName: 'S.', country: 'Peru', continent: 'Americas', age: 30, language: 'C' },
+  { firstName: 'Agustin', lastName: 'V.', country: 'Uruguay', continent: 'Americas', age: 19, language: 'JavaScript' },
+  { firstName: 'Precious', lastName: 'G.', country: 'South Africa', continent: 'Africa', age: 22, language: 'JavaScript' },
+  { firstName: 'Nikau', lastName: 'R.', country: 'New Zealand', continent: 'Oceania', age: 39, language: 'Ruby' }
+];
+Notes:
+
+The input array will always be valid and formatted as in the example above.
+The array does not include developers coding in the same language and sharing the same name.*/
+
+function sortByLanguage(list) {
+    return list.sort((a, b) => {
+        const aLanguage = a.language.toLowerCase();
+        const bLanguage = b.language.toLowerCase();
+        const aFirstName = a.firstName.toLowerCase();
+        const bFirstName = b.firstName.toLowerCase();
+
+        if (aLanguage === bLanguage) {
+            if (aFirstName < bFirstName) return -1;
+            if (aFirstName > bFirstName) return 1;
+            return 0;
+        } else {
+            if (aLanguage < bLanguage) return -1;
+            if (aLanguage > bLanguage) return 1;
+            return 0;
+        }
+    });
+}
+
+const list1 = [
+    {
+        firstName: 'Nikau',
+        lastName: 'R.',
+        country: 'New Zealand',
+        continent: 'Oceania',
+        age: 39,
+        language: 'Ruby',
+    },
+    {
+        firstName: 'Precious',
+        lastName: 'G.',
+        country: 'South Africa',
+        continent: 'Africa',
+        age: 22,
+        language: 'JavaScript',
+    },
+    {
+        firstName: 'Maria',
+        lastName: 'S.',
+        country: 'Peru',
+        continent: 'Americas',
+        age: 30,
+        language: 'C',
+    },
+    {
+        firstName: 'Agustin',
+        lastName: 'V.',
+        country: 'Uruguay',
+        continent: 'Americas',
+        age: 19,
+        language: 'JavaScript',
+    },
+];
+
+console.log(sortByLanguage(list1));
