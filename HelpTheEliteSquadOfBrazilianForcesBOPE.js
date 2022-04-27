d6abe1d2d17add8e3aef8e6f195c1a6c5c75aa57
torokattila <torcsiattila93@gmail.com>
2022-04-27 19:52:30 +0200

    Help the Elite Squad of Brazilian forces BOPE kata is done (8 kyu)

---
+++ HelpTheEliteSquadOfBrazilianForcesBOPE.js
@@ -0,0 +1,35 @@
+/*The BOPE is the squad of special forces of police that usually handles the operations in the Favelas in Rio de Janeiro.
+
+In this Kata you have to write a function that determine the number of magazines that every soldier has to have in his bag.
+
+You will receive the weapon and the number of streets that they have to cross. 
+Considering that every street the officer shoots 3 times. Bellow there is the relation of weapons:
+
+PT92 - 17 bullets
+M4A1 - 30 bullets
+M16A2 - 30 bullets
+PSG1 - 5 bullets
+
+Example:
+
+["PT92", 6] => 2 (6 streets 3 bullets each)
+["M4A1", 6] => 1
+
+The return Will always be an integer so as the params.
+*/
+
+const WEAPONS = {
+    PT92: 17,
+    M4A1: 30,
+    M16A2: 30,
+    PSG1: 5
+};
+
+function magNumber(info) {
+    const weapon = info[0];
+    const streets = info[1];
+
+    return Math.ceil(streets * 3 / WEAPONS[weapon]);
+}
+
+console.log(magNumber(['PT92', 19]));
