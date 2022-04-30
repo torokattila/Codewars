751e641172a3f1847bcaf9e02f27d2b959c05cc2
torokattila <torcsiattila93@gmail.com>
2022-04-30 19:07:24 +0200

    Insert value into an array kata is done (6 kyu)

---
+++ InsertValueIntoAnArray.js
@@ -0,0 +1,30 @@
+/*Extend the Array object with the function insert(index,value). This function must change the original 
+array and insert the value at the given index in the array.
+
+If the index is greater than the array's size, insert the value at the end.
+
+You must return the array to be able to chain the insert function call.
+
+You can use Array's built-in functions to help you.
+
+The 'insert' function must not be enumerable.
+
+Example :
+
+[1,2,3].insert(0,42) => [42,1,2,3]*/
+
+Object.defineProperty(Array.prototype, 'insert', {
+    enumerable: false,
+    writable: true,
+    value: function (index, number) {
+        if (index > this.length) {
+            this.splice(index, this.indexOf([this.length - 1]), number);
+        } else {
+            this.splice(index, 0, number);
+        }
+
+        return this;
+    },
+});
+
+console.log([1, 2, 3].insert(0, 42));
