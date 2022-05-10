6fe0c920362477e1fe7c21c9e40f16da61dabca8
torokattila <torcsiattila93@gmail.com>
2022-05-10 21:01:17 +0200

    Consecutive Differences kata is done (7 kyu)

---
+++ ConsecutiveDifferences.js
@@ -0,0 +1,28 @@
+/*Given a list of integers, find the positive difference between each consecutive pair of numbers, and put this into a new list of differences. 
+Then, find the differences between consecutive pairs in this new list, and repeat until the list has a length of 1. Then, return the single value.
+
+The list will only contain integers, and will not be empty.
+
+For example:
+
+differences([1, 2, 3]) => [1, 1] => [0] -> 0
+differences([1, 5, 2, 7, 8, 9, 0]) => [4, 3, 5, 1, 1, 9] => [1, 2, 4, 0, 8] => .. => 1
+differences([2, 3, 1]) => [1, 2] => [1] => 1*/
+
+function differences(a) {
+    let diffs = a;
+
+    while (diffs.length > 1) {
+        const newArray = [];
+
+        for (let i = 1; i < diffs.length; i++) {
+            newArray.push(Math.abs(diffs[i] - diffs[i - 1]));
+        }
+
+        diffs = newArray;
+    }
+
+    return diffs[0];
+}
+
+console.log(differences([1, 5, 2, 7, 8, 9, 0]));
