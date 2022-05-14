64316fcfe184c747c43a8dc7070b521a3e08101c
torokattila <torcsiattila93@gmail.com>
2022-05-14 14:24:55 +0200

    The reject() function kata is done (7 kyu)

---
+++ TheRejectFunction.js
@@ -0,0 +1,9 @@
+/*Implement a function which filters out array values which satisfy the given predicate.
+
+reject([1, 2, 3, 4, 5, 6], (n) => n % 2 === 0)  =>  [1, 3, 5]*/
+
+function reject(array, predicate) {
+    return array.filter((item) => !predicate(item));
+}
+
+console.log(reject([1, 2, 3, 4, 5, 6], (n) => n % 2 === 0));
