2ee8dea4e45bae49b54fc81e9bcd046d2954bbd9
torokattila <torcsiattila93@gmail.com>
2022-06-07 20:37:15 +0200

    Find all pairs kata is done (7 kyu)

---
+++ FindAllPairs.js
@@ -0,0 +1,30 @@
+/*You are given array of integers, your task will be to count all pairs in that array and return their count.
+
+Notes:
+
+Array can be empty or contain only one value; in this case return 0
+If there are more pairs of a certain number, count each pair only once. E.g.: for [0, 0, 0, 0] the return value is 2 (= 2 pairs of 0s)
+Random tests: maximum array length is 1000, range of values in array is between 0 and 1000
+Examples
+[1, 2, 5, 6, 5, 2]  -->  2
+...because there are 2 pairs: 2 and 5
+
+[1, 2, 2, 20, 6, 20, 2, 6, 2]  -->  4
+...because there are 4 pairs: 2, 20, 6 and 2 (again)*/
+
+function duplicates(array) {
+    const occurrences = {};
+    let result = 0;
+
+    array.forEach((num) => {
+        return occurrences[num] ? occurrences[num]++ : (occurrences[num] = 1);
+    });
+
+    for (const key in occurrences) {
+        result += Math.floor(occurrences[key] / 2);
+    }
+
+    return result;
+}
+
+console.log(duplicates([1, 2, 5, 6, 5, 2]));
