62852b85e3d4b688bc56d3714dab740f8ba8e58c
torokattila <torcsiattila93@gmail.com>
2022-06-07 20:55:06 +0200

    Gryffindor vs Slytherin Quidditch Game kata is done (7 kyu)

---
+++ GryffindorVsSlytherinQuidditchGame.js
@@ -0,0 +1,31 @@
+/*It's the most hotly anticipated game of the school year - Gryffindor vs Slytherin! 
+Write a function which returns the winning team.
+
+You will be given two arrays with two values.
+
+The first given value is the number of points scored by the team's 
+Chasers and the second a string with a 'yes' or 'no' value if the team caught the golden snitch!
+
+The team who catches the snitch wins their team an extra 150 points - but doesn't always win them the game.
+
+gameWinners([150, 'yes'],[200, 'no']) //'Gryffindor wins!'
+gameWinners([400, 'no'],[350, 'yes']) //'Slytherin wins!'
+If the score is a tie return "It's a draw!""
+
+** The game only ends when someone catches the golden snitch, so one array will always include 'yes' or 'no.' 
+Points scored by Chasers can be any positive integer.*/
+
+function gameWinners(gryffindor, slytherin) {
+    let gryffPoints =
+        gryffindor[1] === 'yes' ? gryffindor[0] + 150 : gryffindor[0];
+    let slythPoints =
+        slytherin[1] === 'yes' ? slytherin[0] + 150 : slytherin[0];
+
+    return gryffPoints === slythPoints
+        ? `It's a draw!`
+        : gryffPoints > slythPoints
+        ? 'Gryffindor wins!'
+        : 'Slytherin wins!';
+}
+
+console.log(gameWinners([400, 'no'], [350, 'yes']));
