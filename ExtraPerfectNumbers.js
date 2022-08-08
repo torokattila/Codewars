5f6cef0307d089686e76fea856ba707129588f40
torokattila <torcsiattila93@gmail.com>
2022-08-08 20:46:25 +0200

    Extra Perfect Numbers (Special Numbers Series #7) kata is done (7 kyu)

---
+++ ExtraPerfectNumbers.js
@@ -0,0 +1,45 @@
+/*Definition
+Extra perfect number is the number that first and last bits are set bits.
+
+Task
+Given a positive integer N , Return the extra perfect numbers in range from 1 to N .
+
+Notes
+Number passed is always Positive .
+
+Returned array/list should contain the extra perfect numbers in ascending order from lowest to highest
+
+Input >> Output Examples
+extraPerfect(3)  ==>  return {1,3}
+Explanation:
+(1)10 =(1)2
+First and last bits as set bits.
+
+(3)10 = (11)2
+First and last bits as set bits.
+
+extraPerfect(7)  ==>  return {1,3,5,7}
+Explanation:
+(5)10 = (101)2
+First and last bits as set bits.
+
+(7)10 = (111)2
+First and last bits as set bits.
+*/
+
+function extraPerfect(n) {
+  const result = [];
+
+  for (let i = 1; i <= n; i++) {
+    const binary = i.toString(2);
+
+    if (binary[0] === binary[binary.length - 1]) result.push(i);
+  }
+
+  return result;
+}
+
+console.log(extraPerfect(3));
+console.log(extraPerfect(5));
+console.log(extraPerfect(7));
+console.log(extraPerfect(28));
