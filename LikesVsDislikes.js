263aac8ebb0b9355f73e602bbabba3237b4896e0
torokattila <torcsiattila93@gmail.com>
2022-08-16 20:20:52 +0200

    Likes Vs Dislikes kata is done (7 kyu)

---
+++ LikesVsDislikes.js
@@ -0,0 +1,50 @@
+/*YouTube had a like and a dislike button, which allowed users to express their opinions about particular content. 
+It was set up in such a way that you cannot like and dislike a video at the same time. 
+There are two other interesting rules to be noted about the interface: Pressing a button, which is already active, will undo your press. 
+If you press the like button after pressing the dislike button, the like button overwrites the previous "Dislike" state. 
+The same is true for the other way round.
+
+Task
+Create a function that takes in a list of button inputs and returns the final state.
+
+Examples
+likeOrDislike([Dislike]) => Dislike
+likeOrDislike([Like,Like]) => Nothing
+likeOrDislike([Dislike,Like]) => Like
+likeOrDislike([Like,Dislike,Dislike]) => Nothing
+Notes
+If no button is currently active, return Nothing.
+If the list is empty, return Nothing.
+*/
+
+function likeOrDislike(buttons) {
+  if (!buttons.length) return 'Nothing';
+  let isLiked = false;
+  let isDisliked = false;
+
+  buttons.forEach((button) => {
+    if (button === 'Like' && isLiked) {
+      isLiked = false;
+    } else if (button === 'Dislike' && isDisliked) {
+      isDisliked = false;
+    } else if (button === 'Dislike' && isLiked) {
+      isLiked = false;
+      isDisliked = true;
+    } else if (button === 'Like' && isDisliked) {
+      isLiked = true;
+      isDisliked = false;
+    } else if (button === 'Like') {
+      isLiked = true;
+    } else if (button === 'Dislike') {
+      isDisliked = true;
+    }
+  });
+
+  return isLiked ? 'Like' : isDisliked ? 'Dislike' : 'Nothing';
+}
+
+console.log(likeOrDislike([]));
+console.log(likeOrDislike(['Dislike']));
+console.log(likeOrDislike(['Dislike', 'Like']));
+console.log(likeOrDislike(['Like', 'Like']));
+console.log(likeOrDislike(['Like', 'Dislike', 'Dislike']));
