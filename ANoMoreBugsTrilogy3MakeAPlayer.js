bcd621e324a84b0aea1509ff2dbc16cef4712e9d
torokattila <torcsiattila93@gmail.com>
2022-08-28 15:55:53 +0200

    A (no more) bugs trilogy: Episode 3 - Make a Player kata is done (7 kyu)

---
+++ ANoMoreBugsTrilogy3MakeAPlayer.js
@@ -0,0 +1,43 @@
+/*Pac succesfully passed the first & second test.
+He just received the last message from the Academy,
+
+This is your last test, if you pass, your application will be accepted. You have 45 minutes.
+
+Good Luck
+
+Pac has to create a Football Player object with the following attributes :
+
+name      -> (string)
+position  -> (string)
+age       -> (int)
+dribbling -> (int)
+pass      -> (int)
+shoot     -> (int)
+Example
+myPlayer = new Player("Dumbo", "Kata Coder", 1,2,3,4);
+
+myPlayer.name => "Dumbo"
+myPlayer.position => "Kata Coder"
+myPlayer.age => 1
+myPlayer.dribbling => 2
+myPlayer.pass => 3
+myPlayer.shoot => 4
+*/
+
+function Player(name, position, age, dribbling, pass, shoot) {
+  this.name = name;
+  this.position = position;
+  this.age = age;
+  this.dribbling = dribbling;
+  this.pass = pass;
+  this.shoot = shoot;
+}
+
+const myPlayer = new Player('Dumbo', 'Kata Coder', 1, 2, 3, 4);
+
+console.log(myPlayer.name);
+console.log(myPlayer.position);
+console.log(myPlayer.age);
+console.log(myPlayer.dribbling);
+console.log(myPlayer.pass);
+console.log(myPlayer.shoot);
