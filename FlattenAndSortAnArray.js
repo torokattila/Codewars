eae5303337929d9c4084f9783d905cb355ed3217
torokattila <torcsiattila93@gmail.com>
2022-09-04 20:21:26 +0200

    Flatten and sort an array kata is done (7 kyu)

---
+++ FlattenAndSortAnArray.js
@@ -0,0 +1,28 @@
+/*Given a two-dimensional array of integers, return the flattened version of the array with all the integers in the sorted (ascending) order.
+
+Example:
+
+Given [[3, 2, 1], [4, 6, 5], [], [9, 7, 8]], your function should return [1, 2, 3, 4, 5, 6, 7, 8, 9].
+*/
+
+function flattenAndSort(array) {
+  const result = [];
+
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array[i].length; j++) {
+      result.push(array[i][j]);
+    }
+  }
+
+  return result.sort((a, b) => a - b);
+}
+
+console.log(flattenAndSort([[], [1]]));
+console.log(
+  flattenAndSort([
+    [3, 2, 1],
+    [7, 9, 8],
+    [6, 4, 5],
+  ])
+);
+console.log(flattenAndSort([[1, 3, 5], [100], [2, 4, 6]]));
