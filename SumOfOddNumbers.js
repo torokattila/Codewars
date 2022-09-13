7011678392c7a3b037923dc0bf8711eb4fb81630
torokattila <torcsiattila93@gmail.com>
2022-09-13 20:53:09 +0200

    Sum of odd numbers kata is done (7 kyu)

---
+++ SumOfOddNumbers.js
@@ -0,0 +1,31 @@
+/*Given the triangle of consecutive odd numbers:
+
+             1
+          3     5
+       7     9    11
+   13    15    17    19
+21    23    25    27    29
+...
+Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: (Input --> Output)
+
+1 -->  1
+2 --> 3 + 5 = 8
+*/
+
+function rowSumOddNumbers(n) {
+  let sum = 0;
+  let counter = n * (n - 1) + 1;
+
+  for (let i = 0; i < n; i++) {
+    sum += counter;
+    counter += 2;
+  }
+
+  return sum;
+}
+
+console.log(rowSumOddNumbers(1));
+console.log(rowSumOddNumbers(2));
+console.log(rowSumOddNumbers(3));
+console.log(rowSumOddNumbers(4));
+console.log(rowSumOddNumbers(42));
