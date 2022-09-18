d4ed4e7dd70813a6a7020345a4f7171d9bf55220
torokattila <torcsiattila93@gmail.com>
2022-09-18 14:50:28 +0200

    The range() function kata is done (6 kyu)

---
+++ TheRangeFunction.js
@@ -0,0 +1,56 @@
+/*Let's implement the range function:
+
+range([start], stop, [step])
+
+range(1, 11);
+=> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+range(1, 4, 0); // /!\ note that the step is 0
+=> [1, 1, 1]
+
+range(0);
+=> []
+
+range(10, 0); // /!\ note that the end is before the start
+=> []
+
+range(10);
+=> [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+range(0, 30, 5);
+=> [0, 5, 10, 15, 20, 25]
+start, if omitted, defaults to 0; step defaults to 1.
+
+Returns a list of integers from start to stop, incremented by step, exclusive.
+
+Note that ranges that stop before they start are considered to be zero-length instead of negative.
+*/
+
+function range(start = 0, end, step = 1) {
+  if (end !== undefined && end < start) return [];
+  const result = [];
+
+  if (end === undefined) {
+    for (let i = 0; i < start; i += step) {
+      result.push(i);
+    }
+  } else if (step === 0) {
+    for (let i = start; i < end; i++) {
+      result.push(start);
+    }
+  } else {
+    for (let i = start; i < end; i += step) {
+      result.push(i);
+    }
+  }
+
+  return result;
+}
+
+console.log(range(10));
+console.log(range(1, 11));
+console.log(range(0, 30, 5));
+console.log(range(1, 4, 0));
+console.log(range(0));
+console.log(range(10, 0));
+console.log(range(0, 5, 0));
