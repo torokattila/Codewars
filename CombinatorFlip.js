11226698143583648562fed31e25151f6521031f
torokattila <torcsiattila93@gmail.com>
2022-10-24 21:05:39 +0200

    Combinator Flip kata is done (6 kyu)

---
+++ CombinatorFlip.js
@@ -0,0 +1,23 @@
+/*Create a combinator function named flip that takes a function as an argument and returns 
+that function with it's arguments reversed.
+
+For example:
+
+flip(print)(4,5) // returns "5 -> 4"
+function print(a,b) {
+  return a + " -> " + b;
+}
+The idea is to reverse any number of arguments using a higher order function, without any concern for the function being passed into it.
+*/
+
+function print(a, b) {
+  return a + ' -> ' + b;
+}
+
+function flip(fn) {
+  return function () {
+    return fn(...[...arguments].reverse());
+  };
+}
+
+console.log(flip(print)(4, 5));
