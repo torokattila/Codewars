ae2815a82b69089d5131793971ce81adfa44f557
torokattila <torcsiattila93@gmail.com>
2023-03-06 17:13:26 +0100

    Plural kata is completed (8 kyu)

---
+++ Plural.js
@@ -0,0 +1,21 @@
+/*
+We need a simple function that determines if a plural is needed or not. 
+It should take a number, and return true if a plural should be used with that number or false if not. 
+This would be useful when printing out a string such as 5 minutes, 14 apples, or 1 sun.
+
+You only need to worry about english grammar rules for this kata, where anything that isn't singular 
+(one of something), it is plural (not one of something).
+
+All values will be positive integers or floats, or zero.
+*/
+
+function plural(n) {
+  return (n === Number(n) && n % 1 !== 0) || n > 1 || n === 0;
+}
+
+console.log(plural(0));
+console.log(plural(0.5));
+console.log(plural(1));
+console.log(plural(100));
+console.log(plural(Infinity));
+console.log(plural(0.04981218421283584));
