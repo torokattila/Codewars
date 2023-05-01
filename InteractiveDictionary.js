27d07178721bf784f91ffa01e672292f7365bc33
Török Attila <torcsiattila93@gmail.com>
2023-05-01 17:19:10 +0200

    Interactive Dictionary kata is completed (7 kyu)

---
+++ InteractiveDictionary.js
@@ -0,0 +1,36 @@
+/*
+In this kata, your job is to create a class Dictionary which you can add words to and their entries. Example:
+
+>>> let d = new Dictionary();
+
+>>> d.newEntry("Apple", "A fruit that grows on trees");
+
+>>> console.log(d.look("Apple"));
+A fruit that grows on trees
+
+>>> console.log(d.look("Banana"));
+Can't find entry for Banana
+*/
+
+class Dictionary {
+  constructor() {
+    this.dictionary = {};
+  }
+
+  newEntry(key, value) {
+    this.dictionary[key] = value;
+  }
+
+  look(key) {
+    return this.dictionary[key] || `Can't find entry for ${key}`;
+  }
+}
+
+let d = new Dictionary();
+
+d.newEntry('Apple', 'A fruit');
+d.newEntry('Soccer', 'A sport');
+
+console.log(d.look('Apple'));
+console.log(d.look('Soccer'));
+console.log(d.look('Hi'));
