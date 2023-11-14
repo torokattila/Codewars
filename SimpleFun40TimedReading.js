c0f6bc096fa4ba71f033b5517d50fa13f92c99e2
Török Attila <torcsiattila93@gmail.com>
2023-11-14 21:28:43 +0100

    Simple Fun #40: Timed Reading

---
+++ SimpleFun40TimedReading.js
@@ -0,0 +1,46 @@
+/*
+Simple Fun #40: Timed Reading
+
+Timed Reading is an educational tool used in many schools to improve and advance reading skills. 
+A young elementary student has just finished his very first timed reading exercise. 
+Unfortunately he's not a very good reader yet, so whenever he encountered a word 
+longer than maxLength, he simply skipped it and read on.
+
+Help the teacher figure out how many words the boy has read by calculating the number 
+of words in the text he has read, no longer than maxLength.
+
+Formally, a word is a substring consisting of English letters, such that characters to 
+the left of the leftmost letter and to the right of the rightmost letter are not letters.
+
+Example
+For maxLength = 4 and text = "The Fox asked the stork, 'How is the soup?'", the output should be 7
+
+The boy has read the following words: "The", "Fox", "the", "How", "is", "the", "soup".
+
+Input/Output
+[input] integer maxLength
+
+A positive integer, the maximum length of the word the boy can read.
+
+Constraints: 1 ≤ maxLength ≤ 10.
+
+[input] string text
+
+A non-empty string of English letters and punctuation marks.
+
+[output] an integer
+
+The number of words the boy has read.
+*/
+
+function timedReading(maxLength, text) {
+  // console.log(maxLength, text);
+  const withoutSpecChars = text.replace(/[^a-zA-Z ]/g, '');
+
+  return withoutSpecChars === ''
+    ? 0
+    : withoutSpecChars.split(' ').filter((word) => word.length <= maxLength)
+        .length;
+}
+
+console.log(timedReading(1, '...'));
