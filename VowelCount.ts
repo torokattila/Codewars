61e081de80b4a705d2ff40f76eec87e77dcbc0fa
Török Attila <torcsiattila93@gmail.com>
2026-05-19 20:30:58 +0200

    Vowel Count typescript (7kyu)

---
+++ VowelCount.ts
@@ -0,0 +1,16 @@
+/*
+Vowel Count
+Return the number (count) of vowels in the given string.
+
+We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+The input string will only consist of lower case letters and/or spaces.
+*/
+
+export class Kata {
+  static getCount(str: string): number {
+    return str.replace(/[^aeiou]/gi, '').length;
+  }
+}
+
+console.log(Kata.getCount('abracadabra'));
