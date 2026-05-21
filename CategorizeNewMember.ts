7e136d6626ccac298d209a4ae4cb832b8f30623e
Török Attila <torcsiattila93@gmail.com>
2026-05-21 20:18:24 +0200

    Categorize New Member typescript

---
+++ CategorizeNewMember.ts
@@ -0,0 +1,56 @@
+/*
+Categorize New Member
+
+The Western Suburbs Croquet Club has two categories of membership, Senior and Open. They would like your 
+help with an application form that will tell prospective members which category they will be placed.
+
+To be a senior, a member must be at least 55 years old and have a handicap greater than 7. 
+In this croquet club, handicaps range from -2 to +26; the better the player the lower the handicap.
+
+Input
+Input will consist of a list of pairs. Each pair contains information for a single potential member. 
+Information consists of an integer for the person's age and an integer for the person's handicap.
+
+Output
+Output will consist of a list of string values (in Haskell and C: Open or Senior) stating whether the 
+respective member is to be placed in the senior or open category.
+*/
+
+export function openOrSenior(data: number[][]): string[] {
+  const result: string[] = [];
+
+  for (const [age, handicap] of data) {
+    if (age >= 55 && handicap > 7) {
+      result.push('Senior');
+    } else {
+      result.push('Open');
+    }
+  }
+
+  return result;
+}
+
+console.log(
+  openOrSenior([
+    [45, 12],
+    [55, 21],
+    [19, -2],
+    [104, 20],
+  ]),
+);
+console.log(
+  openOrSenior([
+    [3, 12],
+    [55, 1],
+    [91, -2],
+    [53, 23],
+  ]),
+);
+console.log(
+  openOrSenior([
+    [59, 12],
+    [55, -1],
+    [12, -2],
+    [12, 12],
+  ]),
+);
